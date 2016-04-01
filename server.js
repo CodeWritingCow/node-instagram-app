32d048f1e1a8321a800bde433ddde92973238378
Gary Pang <gary.pang@gmail.com>
2016-04-01 01:04:26 -0400

    Add client credentials
    Add Instagram client credentials and access token (as environmental
    variables) to server.js. Add Instagram data to index.ejs. Photos and
    accompanying info now appear in view.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var express = require('express'),
 	app = express(),
 	ig = require('instagram-node').instagram();
 
+
 //	CONFIGURE THE APP
 //	=============================================
 //	tell node where to look for site resources
@@ -15,18 +16,30 @@ app.use(express.static(__dirname + '/public'));
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// TODO configure instagram app with client-id
+// configure instagram app with client-id
+ig.use({
+	access_token: process.env.ACCESS_TOKEN,
+	client_id: process.env.CLIENT_ID,
+	client_secret: process.env.CLIENT_SECRET
+});
+
 
 //	SET THE ROUTES
 //	=============================================
 //	home page route - popular images
 app.get('/', function(req, res) {
 
-	// use the instagram package to get popular images
-	// render the home page and pass in the popular images
-	res.render('pages/index');
+	// add access token
+	ig.use({ access_token: process.env.ACCESS_TOKEN });
 
+	// use the instagram package to get popular images
+	ig.media_popular(function(err, medias, remaining, limit) {
+
+		// render the home page and pass in the popular images
+		res.render('pages/index', { grams: medias });
+	});
 });
+
 
 //	START THE SERVER
 //	=============================================
