3dc2c06c9f7d2a33abf417f8967dd35140ec975c
Gary Pang <gary.pang@gmail.com>
2016-04-06 01:02:31 -0400

    Install gulp, dotenv
    Add Gulp and dotenv (environmental variable manager) via npm. Save
    Instagram access token and other info in .env file. Add file directories
    to gitignore.

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+// load gulp plugins
+var gulp = require('gulp'),
+	cleanCSS = require('gulp-clean-css'),
+	rename = require('gulp-rename');
+
+// task for minifying css
+gulp.task('minify-css', function() {
+	// grab css files, minify them, save as *.min.css
+	return gulp.src('public/css/*.css')
+			   .pipe(cleanCSS())
+			   .pipe(rename({ suffix: '.min' }))
+			   .pipe(gulp.dest('public/dist/css'));
+});
+
+// TODO: task for minifying JavaScript
